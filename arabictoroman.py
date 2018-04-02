78767e48a7f28ac6f347090a97bcf1875816ef89
pleventhal <37968811+pleventhal@users.noreply.github.com>
2018-04-02 14:01:23 -0700

    Create arabictoroman.py

---
+++ arabictoroman.py
@@ -0,0 +1,21 @@
+array = [['','I','II','III','IV','V','VI','VII','VIII','IV'],
+		 ['','X','XX','XXX','XL','L','LX','LXX','LXXX','LC'],
+		 ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+		 ['','M','MM','MMM']]
+		 
+while True:
+
+	number = input('What is the Arabic Number: ')
+	numlength = len(number)
+	if int(number) == 0:
+		break;
+
+	while numlength < 4:
+		number = '0' + number 
+		numlength = numlength + 1
+
+	lst = [int(i) for i in str(number)]
+
+	romannumeral = array[3][lst[0]] + array[2][lst[1]] + array[1][lst[2]] + array[0][lst[3]]
+
+	print(romannumeral)
